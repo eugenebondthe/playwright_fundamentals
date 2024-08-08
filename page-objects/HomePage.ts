7c7d2e549be3bf08808640934deee4f9a2f680e8
Eugene Bondarenko <eugenebondthe@gmail.com>
2024-08-08 14:12:31 +0200

    Search scenario refactored with POM patern

--- page-objects/HomePage.ts
+++ page-objects/HomePage.ts
@@ -3,10 +3,12 @@ import { expect, Locator, Page } from '@playwright/test'
 export class HomePage {
     readonly page: Page
     readonly signInBtn: Locator
+    readonly searchBox: Locator
 
     constructor(page: Page) {
         this.page = page
         this.signInBtn = page.locator('#signin_button')
+        this.searchBox = page.locator('#searchTerm')
     }
 
     async visit() {
@@ -15,5 +17,10 @@ export class HomePage {
 
     async clickOnSignIn() {
         await this.signInBtn.click()
+    }
+
+    async searchFor(phrase: string) {
+        await this.searchBox.fill(phrase)
+        await this.page.keyboard.press('Enter')
     }
 }
