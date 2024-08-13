c142734f81367e7cfc532fdf37a54d6a561c3f3b
Eugene Bondarenko <eugenebondthe@gmail.com>
2024-08-13 17:33:04 +0200

    Payment test POM integration

--- tests/e2e/e2e-payment.spec.ts
+++ tests/e2e/e2e-payment.spec.ts
@@ -1,14 +1,20 @@
 import { test, expect } from '@playwright/test'
 import { HomePage } from '../../page-objects/HomePage'
 import { LoginPage } from '../../page-objects/LoginPage'
+import { PaymentPage } from '../../page-objects/PaymentPage'
+import { Navbar } from '../../page-objects/components/Navbar'
 
 test.describe.parallel('New Payment', () => {
   let homePage: HomePage
   let loginPage: LoginPage
+  let paymentPage: PaymentPage
+  let navbar: Navbar
 
   test.beforeEach(async ({ page }) => {
     homePage = new HomePage(page)
     loginPage = new LoginPage(page)
+    paymentPage = new PaymentPage(page)
+    navbar = new Navbar(page)
 
     await homePage.visit()
     await homePage.clickOnSignIn()
@@ -17,18 +23,14 @@ test.describe.parallel('New Payment', () => {
   })
 
   test('Should send new payment', async ({ page }) => {
-    await page.click('#pay_bills_tab')
-    await page.selectOption('#sp_payee', 'apple')
-    await page.click('#sp_get_payee_details')
-    await page.waitForSelector('#sp_payee_details')
-    await page.selectOption('#sp_account', '6')
-    await page.fill('#sp_amount', '5000')
-    await page.fill('#sp_date', '2021-11-09')
-    await page.fill('#sp_description', 'Some random message')
-    await page.click('#pay_saved_payees')
-
-    const msg = await page.locator('#alert_content > span')
-    await expect(msg).toBeVisible()
-    await expect(msg).toContainText('The payment was successfully submitted')
+    await navbar.clickOnTab('Pay Bills')
+    await paymentPage.createPayment(
+      'apple',
+      '6',
+      '5000',
+      '2021-11-09',
+      'Some random message',
+    )
+    await paymentPage.assertSuccessMessage()
   })
 })
