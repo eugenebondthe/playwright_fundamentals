3da811b0c2797559e77dad780624d9ec93b9418d
Eugene Bondarenko <eugenebondthe@gmail.com>
2024-08-13 16:45:28 +0200

    Login hook refactoring transfer to POM

--- page-objects/LoginPage.ts
+++ page-objects/LoginPage.ts
@@ -18,10 +18,6 @@ export class LoginPage {
   }
 
   //Define login page methods
-  // async visit() {
-  //     await this.page.goto('http://zero.webappsecurity.com/index.html')
-  // }
-
   async login(username: string, password: string) {
     await this.usernameInput.fill(username)
     await this.passwordInput.fill(password)
--- tests/e2e/e2e-submit-form.spec.ts
+++ tests/e2e/e2e-submit-form.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 import { HomePage } from '../../page-objects/HomePage'
 import { FeedbackPage } from '../../page-objects/FeedbackPage'
 
-test.describe.parallel.only('Feedback Form', () => {
+test.describe.parallel('Feedback Form', () => {
   let homePage: HomePage
   let feedbackPage: FeedbackPage
 
