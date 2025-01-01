15be928ac9de69791e882eaaebf5fc53c4c0f036
Eugene Bondarenko <eugenebondarenko@icloud.com>
2025-01-01 18:04:19 +0100

    new tips added

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -1,9 +1,15 @@
 import { test, expect } from '@playwright/test'
+import { getRandomNumber, getRandomString } from '../../utils/data-helpers'
 
 test.describe.only('Tips & tricks section', () => {
-  test('TestInfo Object', async ({ page }, testInfo) => {
+  test.only('TestInfo Object', async ({ page }, testInfo) => {
     await page.goto('https://www.example.com')
     console.log(testInfo.expectedStatus)
+    let newNumber = await getRandomNumber()
+    let newString = await getRandomString()
+
+    console.log(newNumber)
+    console.log(newString)
   })
 
   test('Test Skip Browser', async ({ page, browserName }) => {
@@ -43,10 +49,13 @@ test.describe.only('Tips & tricks section', () => {
     const context = await browser.newContext()
     const page1 = await context.newPage()
     const page2 = await context.newPage()
-    await page1.goto("https://www.example.com")
-    await page2.goto("https://www.example.com")
+    await page1.goto('https://www.example.com')
+    await page2.goto('https://www.example.com')
     await page1.waitForTimeout(5000)
   })
 })
 
 // To select retries number we also can use --retries=number flag in the execution script as well as in config
+// Device emulation with a playwright: npx playwright open --device="iPhone 11" wikipedia.org
+// Easily create a PDF file for ur testing purpose: npx playwright pdf https://www.example.com my-file.pdf
+// Changing a timezone and/or language can be easily done with PW: npx playwright open --timezone="Europe/Rome" --lang="it-IT" google.com
